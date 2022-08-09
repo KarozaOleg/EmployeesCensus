f6aa5d8afa86f7ea07d9bf7db6c2a354f709781c
Oleg Karoza <karoza.oleg@gmail.com>
2022-08-09 15:35:23 +0300

    added autocomplete search for field FirstName on pages Add, Edit

---
+++ EmployeesCensus/Controllers/AutocompleteSearchController.cs
@@ -0,0 +1,22 @@
+﻿using EmployeesCensus.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace EmployeesCensus.Controllers
+{
+    public class AutocompleteSearchController : Controller
+    {
+        EmployeesCensusContext db = new EmployeesCensusContext();
+
+        public ActionResult FirstName(string term)
+        {
+            var termInLowerCase = term.ToLower();
+            var prefilledFirstName = db.Employees
+               .Where(a => a.FirstName.ToLower().Contains(termInLowerCase))
+               .Select(a => new { value = a.FirstName })
+               .Distinct();
+
+            return Json(prefilledFirstName, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- EmployeesCensus/Controllers/HomeController.cs
+++ EmployeesCensus/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
 ﻿using EmployeesCensus.Models;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace EmployeesCensus.Controllers
